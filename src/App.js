ad10448a23d768ef94bdeb9d148bd278bba2617b
Mateo Enrico <mateoenrico1221@gmail.com>
2025-10-19 21:19:20 -0300

    formulario para agregar tarjetas de producto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [productos, setProductos] = useState([]);
+
+  const agregarProducto = () => {
+    setProductos([...productos, { id: Date.now() }]);
+  };
+
   return (
     <div>
       <header>
-        <a href="" className='cont-logo'><img src="/Gastoico.png" alt="Logo Gastoico" className='logo' /></a>
+        <a href="" className='cont-logo'>
+          <img src="/Gastoico.png" alt="Logo Gastoico" className='logo' />
+        </a>
         <a href='#productos' className='navbar'>Productos</a>
         <a href='#gastos-fijos' className='navbar'>Gastos fijos</a>
         <a href='#mano-obra' className='navbar'>Mano de obra</a>
         <a href='#materia-prima' className='navbar'>Materia prima</a>
         <a href='#impuestos' className='navbar'>Impuestos</a>
         <a href='#servicios' className='navbar'>Servicios</a>
-        <a href="" className='cont-login'><img src="/Login.png" alt="" className='login'/> Iniciar Sesión </a>
+        <a href="" className='cont-login'>
+          <img src="/Login.png" alt="" className='login'/> Iniciar Sesión 
+        </a>
       </header>
+
       <div className='contenido'>
         <section id="productos">
-         <h1>Productos</h1>
-         <div className='cont-prod'>
-          <p>Hola soy un producto</p>
-         </div>
+          <h1>Productos</h1>
+
+          <div className='prod-container'>
+            {productos.map((producto) => (
+              <div key={producto.id} className='cont-prod'>
+                <form onSubmit={(e) => e.preventDefault()}>
+                  <input type="text" placeholder="Nombre del producto" />
+                  <input type="number" placeholder="Precio" />
+                  <button type="button">Guardar</button>
+                </form>
+              </div>
+            ))}
+
+            <button className='agreProd' onClick={agregarProducto}>+</button>
+          </div>
         </section>
       </div>
     </div>
@@ -26,3 +49,4 @@ function App() {
 }
 
 export default App;
+
